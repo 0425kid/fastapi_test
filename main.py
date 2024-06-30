fa67abb793421cfdc577474f88c3ca664adbfcd4
0425kid <76833239+0425kid@users.noreply.github.com>
2024-06-30 14:43:03 +0900

    bug fix

--- main.py
+++ main.py
@@ -74,8 +74,3 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
-
-@app.get("/users")
-def read_users(db: Session = Depends(get_db)):
-    users = db.query(User).all()
-    return users
