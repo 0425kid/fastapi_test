604fdffe5be96bdd24ddebd6e247f0764b6b2fcb
0425kid <76833239+0425kid@users.noreply.github.com>
2024-06-30 14:26:54 +0900

    db url rollback

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ from sqlalchemy.orm import sessionmaker, Session
 from datetime import datetime
 import os
 
-#DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://myuser:password@localhost/mydatabase')
-DATABASE_URL = 'postgresql://postgres:passwordd@localhost/mydatabase'
-
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://myuser:password@localhost/mydatabase')
 
 # 데이터베이스 연결 설정
 engine = create_engine(DATABASE_URL)
